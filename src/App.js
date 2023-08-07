a8b6639d182cdc48259b269b258b500d8231d1fd
hari-testpress <hari@testpress.in>
2023-08-07 19:57:06 +0530

    Integrate testpress embed with player SDK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect} from 'react';
 
 function App() {
+  const iframeRef = useRef("null");
+  let player;
+
+  useEffect(() => {
+    if (typeof window.Testpress !== 'undefined') {
+      player = new window.Testpress.Player(iframeRef.current);
+    }
+  }, []);
+
+  const playVideo = () => {
+    player.play()
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className='container'
+        style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <iframe
+          ref={iframeRef}
+          width="560"
+          height="315"
+          src="https://lmsdemo.testpress.in/embed/z1TLpfuZzXh/?access_token=5c49285b-0557-4cef-b214-66034d0b77c3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope picture-in-picture"></iframe>
+        <button onClick={playVideo} style={{ marginTop: "10px" }}>Play Video</button>
+      </div>
+      
     </div>
   );
 }
